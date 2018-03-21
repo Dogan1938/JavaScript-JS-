2e5e1f77c991091487cdb2ac22bd3853c754b2f0
Dogan_M <doganmermertas@hotmail.de>
2018-03-21 21:58:48 +0100

    Corrected task 4

---
+++ Lesson_01/Task_05_CalcLeapYears.js
@@ -0,0 +1,25 @@
+/*
+Taskname:       Calculation of leap years
+Description:    isLeap() calculates if param is leap, 
+                isLeapArray() iterates trough all elements & prints.
+*/
+
+//DEFINITIONS
+function isLeap(num){
+    if(num % 4 == 0){
+        return true;
+    }
+    return false;
+}
+
+function isLeapArray(arr){
+    arr.forEach(element => {
+        console.log(element + ": " + isLeap(element));
+    });
+}
+
+
+
+//MAIN
+let arr1 = [2012,1904,2048,1905];
+isLeapArray(arr1);
