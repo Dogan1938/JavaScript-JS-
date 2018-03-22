02bd922a323d86fc374af1e62c7bb7ca0ac13f99
Dogan_M <doganmermertas@hotmail.de>
2018-03-22 09:34:20 +0100

    Lesson 2 Init task 1 finished

--- Lesson_02/Task_01_HexValidation.js
+++ Lesson_02/Task_01_HexValidation.js
@@ -5,14 +5,38 @@ Description:
 
 //DEFINITIONS
 
+Array.prototype.isHexArray = function (){
+    this.forEach(element => {
+        console.log(element + ": " + element.isHex())
+    })
+}
+
 String.prototype.isHex = function () {
-    let hexAlphabet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexAlphabet = [0, "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
     let hexChar;
 
-    for(let i = 1;i<= this.length;i++){
-        hexChar = false;
-        if(hexAlphabet.find(this[i]) !== undefined){
-            break;
+    lbl1:
+    for (let i = 1; i < this.length; i++) {
+        //hexChar = false;
+        let c = this.charAt(i);
+        if (typeof c === "string") {
+            c = c.toUpperCase();
         }
+        else{
+            c = c.toString();
+            c = c.toUpperCase();
+        }
+        if (hexAlphabet.indexOf(c) !== -1) {
+            continue lbl1;
+        }
+        return false;
     }
+    return true;
 }
+
+
+//MAIN
+arr1 = ["ffx5","gfa5","ff95","af75","339468763afef5","0a5","0"];
+
+arr1.isHexArray();
+
