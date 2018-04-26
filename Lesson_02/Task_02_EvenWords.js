de0ded6096979550459ab2a78308a3e7e7662e61
Dogan1938 <dogan@extremail.ru>
2018-04-26 22:54:43 +0200

    Completed Lesson 2 Task 2

--- Lesson_02/Task_02_EvenWords.js
+++ Lesson_02/Task_02_EvenWords.js
@@ -5,25 +5,51 @@ Description:    Tests if words of given String have an even amount of chars.
 */
 
 //DEFINITIONS
-function evenWords(str) {
+String.prototype.evenWordsV2 = function () {
+    let ret_str = "";
+    let tmp_str = "";
     //Remove all unwanted symbols
-    str = str.replace(/\!|\?|\.|\,|\_/gi, "");
-
-    //Add chars for even words
-    let count = 0;
-    for (let i = 1; i < str.length; i++) {
-        if (str.charAt(i) != " ") {
-            count++;
+    tmp_str = this.replace(/\!|\?|\.|\,|\_/gi, "");
+    //Add last char to uneven words
+    tmp_str.split(" ").forEach(function (element) {
+        if (element.length % 2 == 1) {
+            ret_str += (element + element.charAt(element.length - 1) + " ")
+        } else {
+            ret_str += element + " "
         }
-    }
-    
-    return str;
+    });
+    return ret_str.slice(0, ret_str.length - 1);
 }
 
 
+
+
+
 //MAIN
-let string1 = "!lel _4 !?AAa!";
-console.log(evenWords(string1));
+let string1 = "How did we end up here? We go?";
+
+console.log("Function result:");
+console.log(string1.evenWordsV2());
+console.log("Expected result:");
+console.log("Howw didd we endd up here We go");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -34,11 +60,54 @@ console.log(evenWords(string1));
 
 
 //BACKUP
-    //let regex = /\!|\?|\.|\,|\_/gi;
-    //let to_exclude = [",", ".", "?", "_", "!"];
+//let regex = /\!|\?|\.|\,|\_/gi;
+//let to_exclude = [",", ".", "?", "_", "!"];
 
-    
-    //let regex = new RegExp("\!|\?|\.|\,|\_", "gi"); 
-    //let regex = new RegExp("\\,\.\_\?\!", "gi"); 
 
-    //str = str.replace(/\!/gi, "");
+//let regex = new RegExp("\!|\?|\.|\,|\_", "gi"); 
+//let regex = new RegExp("\\,\.\_\?\!", "gi"); 
+
+//str = str.replace(/\!/gi, "");
+
+
+
+/*
+String.prototype.isEvenV2 = function(){
+    if (this.length % 2 == 0){
+        return true;
+    }
+    return false;
+}
+*/
+
+
+
+
+/*
+console.log("Now the arrow iteration: ");
+string1.split(" ").forEach(element => console.log(element));
+console.log("");
+
+console.log("Now the callback iteration");
+string1.split(" ").forEach(function (element) {
+    console.log(element)
+});
+console.log("");
+*/
+
+
+/*
+function evenWordsV1(str) {
+    //Remove all unwanted symbols
+    str = str.replace(/\!|\?|\.|\,|\_/gi, "");
+
+    //Add chars for uneven words
+    let count = 0;
+    for (let i = 1; i < str.length; i++) {
+        if (str.charAt(i) != " ") {
+            count++;
+        }
+    }
+    return str;
+}
+*/
