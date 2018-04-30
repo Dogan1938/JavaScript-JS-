5110faddcf808ba165d8848762bdfc00855946a6
Dogan1938 <dogan@extremail.ru>
2018-05-01 00:25:30 +0200

    Save

---
+++ Lesson_02/Task_05_Snake.js
@@ -0,0 +1,12 @@
+/*
+Taskname:       Remove Minimum
+Description:    Removes the miminum Number in an array, if inludes(element)
+                == true for more than 1 Elements, remove the one with 
+                lowest index. if array Empty, return empty array.
+                Order of elements are immutable.
+*/
+
+//DEFINITIONS
+
+
+var snake_table = document.createElement("tr");
